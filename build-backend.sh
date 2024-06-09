12871e2e5de3efddcfe472a1e5f949eb74b4fc8f
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-06-09 17:54:04 +0700

    perbaiki

--- build-backend.sh
+++ build-backend.sh
@@ -1,6 +1,6 @@
 docker stop backend-apps 
 docker rm backend-apps   
 docker rmi backend-server 
-docker build -t backend-server . -f backend-v2/Dockerfile
+docker build -t backend-server . -f backend/Dockerfile
 docker create -it --name backend-apps -p 3300:3300 backend-server 
 docker start backend-apps 
