b4224c54a4063312923be687c43bb33a41ee2b4e
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-07-29 11:51:44 +0700

    update data

--- backend/index.js
+++ backend/index.js
@@ -17,93 +17,93 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-global.client = new Client({
-    restartOnAuthFail: true,
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-    // authStrategy: new LocalAuth()
-    authStrategy: new NoAuth()
-});
+// global.client = new Client({
+//     restartOnAuthFail: true,
+//     puppeteer: {
+//         args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//     },
+//     // authStrategy: new LocalAuth()
+//     authStrategy: new NoAuth()
+// });
 
-global.authed = false;
+// global.authed = false;
 
-// When the client is ready, run this code (only once)
-client.once('ready', async () => {
-    try {
-        await arduinoService.sendWhatsapp();
-        console.log('Client is ready!');
-    } catch (error) {
-        console.error('Error during ready event:', error);
-    }
-});
+// // When the client is ready, run this code (only once)
+// client.once('ready', async () => {
+//     try {
+//         await arduinoService.sendWhatsapp();
+//         console.log('Client is ready!');
+//     } catch (error) {
+//         console.error('Error during ready event:', error);
+//     }
+// });
 
-// When the client received QR-Code
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-    fs.writeFileSync("./services/last.qr", qr);
-    console.log('QR RECEIVED', qr);
-});
-
-
-client.on('message', async message => {
-    try {
-        if (message.body === '!ping') {
-            await client.sendMessage(message.from, 'pong');
-        }
-        console.log(message.body);
-    } catch (error) {
-        console.error('Error handling message:', error);
-    }
-});
+// // When the client received QR-Code
+// client.on('qr', (qr) => {
+//     qrcode.generate(qr, {small: true});
+//     fs.writeFileSync("./services/last.qr", qr);
+//     console.log('QR RECEIVED', qr);
+// });
 
 
-// Start your client
-// client.initialize();
+// client.on('message', async message => {
+//     try {
+//         if (message.body === '!ping') {
+//             await client.sendMessage(message.from, 'pong');
+//         }
+//         console.log(message.body);
+//     } catch (error) {
+//         console.error('Error handling message:', error);
+//     }
+// });
 
-client.on("disconnected", () => {
+
+// // Start your client
+// // client.initialize();
+
+// client.on("disconnected", () => {
    
 
-    // fs.unlink(filePath, (err) => {
-    //     if (err) {
-    //       console.error(`Error removing file: ${err}`);
-    //       return;
-    //     }
+//     // fs.unlink(filePath, (err) => {
+//     //     if (err) {
+//     //       console.error(`Error removing file: ${err}`);
+//     //       return;
+//     //     }
       
-    //     console.log(`File ${filePath} has been successfully removed.`);
-    //   });
-    console.log("disconnected");
-    // initializeClient();
-    // process.exit(1);
-});
+//     //     console.log(`File ${filePath} has been successfully removed.`);
+//     //   });
+//     console.log("disconnected");
+//     // initializeClient();
+//     // process.exit(1);
+// });
 
 
-client.on('loading_screen', (percent, message) => {
-    console.log('LOADING SCREEN', percent, message);
-});
+// client.on('loading_screen', (percent, message) => {
+//     console.log('LOADING SCREEN', percent, message);
+// });
 
-async function initializeClient(retries = 3) {
-    while (retries > 0) {
-        try {
-            await client.initialize();
-            return;
-        } catch (error) {
-            console.error('Error initializing client:', error);
-            retries -= 1;
-            if (retries > 0) {
-                console.log(`Retrying... (${retries} attempts left)`);
-            } else {
-                throw new Error('Failed to initialize client after multiple attempts');
-            }
-        }
-    }
-}
+// async function initializeClient(retries = 3) {
+//     while (retries > 0) {
+//         try {
+//             await client.initialize();
+//             return;
+//         } catch (error) {
+//             console.error('Error initializing client:', error);
+//             retries -= 1;
+//             if (retries > 0) {
+//                 console.log(`Retrying... (${retries} attempts left)`);
+//             } else {
+//                 throw new Error('Failed to initialize client after multiple attempts');
+//             }
+//         }
+//     }
+// }
 
 
 
-initializeClient().catch(error => {
-    console.error('Client initialization failed:', error);
-});
+// initializeClient().catch(error => {
+//     console.error('Client initialization failed:', error);
+// });
 
 const authRoute = require("./services/auth");
 const chatRoute = require("./services/chatting");
