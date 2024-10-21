0403bad85c6eda43a913e7c3ef68f13f497d69ac
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-10-21 14:04:56 +0700

    update data

--- backend/database/migrations/20240609031031-create-sensor-check.js
+++ backend/database/migrations/20240609031031-create-sensor-check.js
@@ -18,11 +18,11 @@ module.exports = {
       kelembapan: {
         type: Sequelize.FLOAT
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
